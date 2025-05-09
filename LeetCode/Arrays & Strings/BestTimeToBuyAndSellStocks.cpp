77ff900c12edddfd7df03d620b5be1b191ec77d3
Thiago Arruda <thiago_arruda@id.uff.br>
2025-05-09 19:32:13 -0300

    📦 NEW: LC - Best Time to Buy and Sell Stock

---
+++ LeetCode/Arrays & Strings/BestTimeToBuyAndSellStocks.cpp
@@ -0,0 +1,36 @@
+#include <iostream>
+#include <vector>
+
+
+using namespace std;
+
+class Solution {
+public:
+    int maxProfit(vector<int>& prices) {
+        int buy_price = prices[0];
+		int profit = 0;
+
+		for (int i = 1; i < prices.size(); i++) {
+			int price = prices[i];
+			
+			if (price < buy_price) {
+				buy_price = price;
+			}
+
+			profit = max(profit, price - buy_price);
+		}
+
+		return profit;
+    }
+};
+
+int main() {
+	Solution solution;
+	vector<int> prices = {7,1,5,3,6,4};
+
+	int maxProfit = solution.maxProfit(prices);
+
+	cout << maxProfit << endl;
+	
+	return 0;
+}
