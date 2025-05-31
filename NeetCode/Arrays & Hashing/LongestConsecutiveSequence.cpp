dcb7ebd046438320c688ed7286c80ec3495c5c44
Thiago <thiago_arruda@id.uff.br>
2025-05-31 10:26:16 -0300

    📦 NEW: NC - Longest Consecutive Sequence

--- NeetCode/Arrays & Hashing/LongestConsecutiveSequence.cpp
+++ NeetCode/Arrays & Hashing/LongestConsecutiveSequence.cpp
@@ -12,19 +12,25 @@ public:
     unordered_set<int> set;
 
     // Iterate over the array, putting nums[i] in set
-    for (int i = 0; i < size; i++) {
+    for (int i = 0; i < size; i++) 
       set.insert(nums[i]);
-    }
+    
 
-    int currentNum;
+    int longest = 0;
     for (int i = 0; i < size; i++) {
       int num = nums[i];
 
       if (set.count(num - 1) == 0) {
         // Num is a start of a sequence
-        currentNum = num;
+        int length = 0;
+
+        while (set.count(num + length) > 0) 
+          length++;
+
+        longest = max(length, longest);
       }
     }
+    return longest;
   }
 };
 
